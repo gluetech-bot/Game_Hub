29f672fb6ca8cba6f0374c202e2e2f052b9244be
shanmukha252007 <25b0907@iitb.ac.in>
2026-03-31 12:03:01 +0530

    Update main.sh
    now verifying for diff users

--- main.sh
+++ main.sh
@@ -1,10 +1,6 @@
-chkusr() {
-if cut -d " " -f1 users.tsv| grep -q "$1"  ; then
-    return 0 ;
-  else
-    read -p $"This username doesn't exists . Do you want to register and update the users.tsv . If you want to , please enter yes or else no : " regis
+chkusr() {                                                                                                              if cut -d " " -f1 users.tsv| grep -q "^$1$"  ; then                                                                         return 0 ;                                                                                                            else                                                                                                                      read -p "This username doesn't exists . Do you want to register and update the users.tsv . If you want to , please enter yes or else no : " regis
     if [[ $regis == "yes" ]] ; then
-      register
+            register $1
     else
       exit
     fi
@@ -13,7 +9,7 @@ if cut -d " " -f1 users.tsv| grep -q "$1"  ; then
 }
 
 register() {
-      read -p "Please enter new username : " newuser
+#      read -p "Please enter new username : " newuser
       while true
       do
       read -p "Set password : " newpass
@@ -25,15 +21,15 @@ register() {
       fi
       done
       hashpass=$(echo $newpass | sha256sum | cut -d " " -f 1)
-      echo "$newuser $hashpass" >> users.tsv
-      echo "$newuser is now registered"
-      exit
+      echo "$1 $hashpass" >> users.tsv
+       echo "$1 is now registered"
+      return 0;
 }
 
 chkpass() {
 while true
 do
-read -p "Enter your password : " pass
+read -p "Enter  password of \"$1\": " pass
 hashpass=$(echo $pass | sha256sum | cut -d " " -f 1)
 if [[ $hashpass == $(awk -v val="$1" 'val==$1{print $2}' users.tsv) ]] ; then
     return 0 ;
@@ -46,8 +42,15 @@ done
 read -p "Player 1 , Enter your username : " user1
 chkusr $user1
 chkpass $user1
-
+while true
+do
 read -p "Player 2 , Enter your username : " user2
+if [[ $user1 != $user2 ]] ;then
+     break
+else
+ echo "enter different username"
+fi
+done
 chkusr $user2
 chkpass $user2
 
